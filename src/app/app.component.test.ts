4d8634e00b40e43093696940feb2bcc33498ecea
Dmitrii Shikhaleev <dmitrii_shikhaleev@epam.com>
2023-09-03 18:43:06 +0300

    sync with main branch

---
+++ src/app/app.component.test.ts
@@ -0,0 +1,22 @@
+import { MockDataService } from './mock-data.service';
+import { AppComponent } from './app.component';
+
+describe("App Component", () => {
+  it("should run test", () => {
+    const mockMockDataService = {
+      getCharacters: (str: string) => str
+    } as unknown as MockDataService;
+
+    const appComponent = new AppComponent(mockMockDataService);
+
+    const inputEvent = {
+      target: {
+        value: "testStr",
+      }
+    };
+
+    appComponent.changeCharactersInput(inputEvent);
+
+
+  })
+});
